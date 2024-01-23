87a0e915132b8af99602e0fcdd4d59f07246267b
loaimasri <loai.masri@foothillsolutions.com>
2024-01-23 15:53:20 +0200

    Refactor Post collection UI configuration

--- tina/collection/post.ts
+++ tina/collection/post.ts
@@ -1,26 +1,32 @@
 import { Collection } from "tinacms";
+import { v4 as uuid } from "uuid";
 
-export const Post : Collection={
-    name: "post",
-    label: "Posts",
-    path: "content/posts",
-    fields: [
-      {
-        type: "string",
-        name: "title",
-        label: "Title",
-        isTitle: true,
-        required: true,
-      },
-      {
-        type: "rich-text",
-        name: "body",
-        label: "Body",
-        isBody: true,
-      },
-    ],
-    ui: {
-      // This is an DEMO router. You can remove this to fit your site
-      router: ({ document }) => `/post/${document._sys.filename}`,
+export const Post: Collection = {
+  name: "post",
+  label: "Posts",
+  path: "content/posts",
+  format: "md",
+
+  ui: {
+    router: ({ document }) => `/post/${document._sys.filename}`,
+    filename: {
+      readonly: true,
+      slugify: ({ title }) =>
+        `${title?.replace(/\s+/g, "-").toLowerCase().slice(0, 20)}`,
     },
-  }
+  },
+  fields: [
+    {
+      type: "string",
+      name: "title",
+      label: "Title",
+      required: true,
+    },
+    {
+      type: "rich-text",
+      name: "body",
+      label: "Body",
+      isBody: true,
+    },
+  ],
+};
