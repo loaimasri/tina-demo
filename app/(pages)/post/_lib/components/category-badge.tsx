e2f413ade98437ec9b466b05ed68a11a0de27ce1
loaimasri <loai.masri@foothillsolutions.com>
2024-02-11 15:00:04 +0200

    Refactor category badge component

--- app/(pages)/post/_lib/components/category-badge.tsx
+++ app/(pages)/post/_lib/components/category-badge.tsx
@@ -12,14 +12,16 @@ export default function CategoryBadge({
 }: CategoryBadgeProps) {
   const [isSelected, setIsSelected] = React.useState(selected);
 
+  const handleSelect = () => setIsSelected(!isSelected);
+
   return (
     <Badge
       variant="secondary"
-      className={`cursor-pointer px-2 py-[0.375em] text-sm hover:bg-primary/95 hover:text-secondary hover:transition-all hover:duration-200  ${
+      className={`cursor-pointer capitalize font-normal px-2 py-[0.375em] text-sm hover:bg-primary/95 hover:text-secondary hover:transition-all hover:duration-200  ${
         isSelected &&
         "bg-primary text-primary-foreground hover:text-primary hover:bg-secondary/90"
       }`}
-      onClick={() => setIsSelected(!isSelected)}
+      onClick={handleSelect}
     >
       {category}
     </Badge>
