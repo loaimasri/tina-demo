63ba8fedeee0ee0e1d384a5eb6507c7ef93f1138
LoaiMasri <loaimasri2002@gmail.com>
2024-02-14 22:36:51 +0200

    include search functionality and improve filtering

--- app/(pages)/post/_lib/containers/post-list.tsx
+++ app/(pages)/post/_lib/containers/post-list.tsx
@@ -2,7 +2,10 @@
 import { type PostConnectionQuery } from "@/tina/__generated__/types";
 import { useTina } from "tinacms/dist/react";
 
+import { Icon } from "@/app/_lib/components/icon";
+import { Input } from "@components/ui/input";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 import { PostCard } from "../components/post-card";
 import { Tag } from "../components/tag";
 
@@ -22,19 +25,26 @@ export function PostList(props: PostListProps): JSX.Element {
   const searchParams = useSearchParams();
   const router = useRouter();
   const pathname = usePathname();
-
   const selectedTags = searchParams?.get("tags")?.split(",") ?? [];
 
-  console.log("selectedTags", selectedTags);
-
-  const tags: string[] = Array.from(
-    new Set(
-      posts?.reduce((acc: string[], curr) => {
-        if (Array.isArray(curr?.node?.tags)) acc.push(...curr?.node?.tags);
-        return acc;
-      }, []),
-    ),
+  const [searchTerm, setSearchTerm] = useState<string>(
+    searchParams?.get("search") ?? "",
   );
+
+  useEffect(() => {
+    setSearchTerm(searchParams?.get("search") ?? "");
+  }, [searchParams]);
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const { value } = e.target;
+    setSearchTerm(value);
+
+    const params = new URLSearchParams(searchParams?.toString());
+
+    if (value) params.set("search", value);
+    else params.delete("search");
+    router.push(`${pathname}?${params.toString()}`);
+  };
 
   const handleSelectTag = (tag: string): void => {
     const newSelectedTags = selectedTags.includes(tag)
@@ -42,17 +52,50 @@ export function PostList(props: PostListProps): JSX.Element {
       : [...selectedTags, tag];
 
     const params = new URLSearchParams(searchParams?.toString());
-    if (newSelectedTags.length > 0) {
+    if (newSelectedTags.length > 0)
       params.set("tags", newSelectedTags.join(","));
-    } else {
-      params.delete("tags");
-    }
+    else params.delete("tags");
+
     const queryString = params.toString();
-    router.push(`${pathname}${queryString ? "?" + queryString : ""}`);
+    router.push(`${pathname}?${queryString}`);
   };
+
+  const filteredPosts = posts
+    ?.filter((post) => {
+      const matchesSearchTerm =
+        post?.node?.title.toLowerCase().includes(searchTerm.toLowerCase()) ??
+        post?.node?.description
+          .toLowerCase()
+          .includes(searchTerm.toLowerCase());
+
+      const matchesTags =
+        selectedTags.length === 0 ||
+        selectedTags.some((tag) => {
+          return post?.node?.tags.includes(tag);
+        });
+
+      return matchesSearchTerm && matchesTags;
+    })
+    .sort(
+      (a, b) =>
+        new Date(b?.node?.publishedAt ?? "").getTime() -
+        new Date(a?.node?.publishedAt ?? "").getTime(),
+    );
+
+  const tags = Array.from(
+    new Set(posts?.flatMap((post) => post?.node?.tags).filter(Boolean) ?? []),
+  ) as string[];
 
   return (
     <section className="flex flex-col gap-8 pt-8">
+      <Input
+        type="search"
+        placeholder="Search something here"
+        icon={<Icon name="search" />}
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+
       <div className="flex flex-wrap gap-x-4 gap-y-2">
         {tags?.map((tag) => (
           <Tag
@@ -65,30 +108,17 @@ export function PostList(props: PostListProps): JSX.Element {
       </div>
 
       <div className="flex flex-col gap-6">
-        {posts
-          ?.filter(
-            (post) =>
-              selectedTags.length === 0 ||
-              post?.node?.tags?.some((tag: string) =>
-                selectedTags.includes(tag),
-              ),
-          )
-          .sort(
-            (a, b) =>
-              new Date(b?.node?.publishedAt ?? "").getTime() -
-              new Date(a?.node?.publishedAt ?? "").getTime(),
-          )
-          .map((post) => (
-            <PostCard
-              key={post?.node?.id}
-              cover={post?.node?.cover ?? ""}
-              title={post?.node?.title ?? ""}
-              tags={post?.node?.tags ?? []}
-              description={post?.node?.description ?? ""}
-              publishedAt={post?.node?.publishedAt ?? ""}
-              author={post?.node?.author ?? ""}
-            />
-          ))}
+        {filteredPosts?.map((post) => (
+          <PostCard
+            key={post?.node?.id}
+            cover={post?.node?.cover ?? ""}
+            title={post?.node?.title ?? ""}
+            tags={post?.node?.tags ?? []}
+            description={post?.node?.description ?? ""}
+            publishedAt={post?.node?.publishedAt ?? ""}
+            author={post?.node?.author ?? ""}
+          />
+        ))}
       </div>
     </section>
   );
