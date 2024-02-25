880162a0e5d880b3a5f1775b1bc22502d294d6f7
NaBuLSiO <119036273+NaBuLSiO@users.noreply.github.com>
2024-02-25 13:53:27 +0200

    Update config.ts

--- config.ts
+++ config.ts
@@ -7,7 +7,7 @@ export const environment = {
   },
 
   mongodb: {
-    uri: "mongodb://localhost:27017/",
+    uri: "mongodb+srv://osamasarawan:UbvnBgGB8HREFJiY@cluster0.m4h5jss.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   },
 
   general: {
