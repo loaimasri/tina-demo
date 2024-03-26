7a6d4e567e9455b3298a6dfdf3927f6b9a602952
Loai Masri <156896065+loaimasri@users.noreply.github.com>
2024-03-26 16:08:19 +0200

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-docker compose up -d mongo --build
+docker run -d --name my-mongo mongo:latest mongod --bind_ip_all --network host
+#docker compose up -d mongo --build
 
 docker compose up -d app --build
