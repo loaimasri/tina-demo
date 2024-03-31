e94ca549660c1308fcd76ddeff410d60b0948787
Loai Masri <loai.masri@foothillsolutions.com>
2024-03-31 16:41:46 +0200

    Add build-and-deploy and run-compose scripts

---
+++ scripts/build-and-deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$1" = "prod" ]; then
+    echo "Setting up MongoDB for production..."
+    docker run -p 27017:27017 -d --name my-mongo-prod -v my-mongo-data-prod:/data/db mongo:latest mongod
+
+    REGISTRY_URL="your.harbor.registry.url/fts-tinacms"
+    DATE=$(date +%F)
+    docker compose build app
+    docker tag fts-tinacms:latest ${REGISTRY_URL}:${DATE}
+
+    echo "Logging in to Harbor..."
+    docker login ${REGISTRY_URL}
+
+    echo "Pushing image to Harbor..."
+    docker push ${REGISTRY_URL}:${DATE}
+else
+    echo "Setting up MongoDB for development..."
+    docker run -p 27017:27017 --rm -d --name my-mongo-dev -v my-mongo-data-dev:/data/db mongo:latest mongod
+
+    docker compose -f ../.docker/docker-compose-build.yml build app
+fi
---
+++ scripts/run-compose.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$1" = "prod" ]; then
+    export APP_IMAGE="your.harbor.registry.url/fts-tinacms:latest"
+else
+    export APP_IMAGE="fts-tinacms"
+fi
+
+docker compose -f ../.docker/docker-compose.yml up
